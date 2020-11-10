ea615f37f9de0594c3dcce46a2100c9885481e79
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-10 13:43:13 +0100

    Vattenkruka Feat: Added functionality for readPackageJson and added a package.Json file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const os = require('os');
 const http = require('http');
 const readline = require('readline');
+const fs = require('fs');
 
 // Setup for localhost 
 const hostname='localhost';
@@ -25,20 +26,29 @@ const optionStartHttpServer = '3. Start HTTP Server';
 
 rl.question('Choose an option: \n 1. Read package.json \n 2. Display OS info \n 3. Start HTTP Server \n Type a number: ', (answer) => {
     
-    console.log()
-
+    //Checks if the answer is a number, or if it is in the interval between 1-3
+if(answer < 1 || answer > 3 || isNaN(answer)){
+    console.log("Invalid input, please input a number between 1-3");
+}
+else{
     switch(answer){
+        //Option for reading the json package
         case '1':
+            readPackageJson();
         break;
+
+        //Option for displaying the OS info
         case '2':
+            readOSInfo();
         break;
+
         //Option for starting a server
         case '3':
             runServer();
         break;
         default:
     }
-
+}
     rl.close();
 });
 
@@ -49,12 +59,14 @@ rl.question('Choose an option: \n 1. Read package.json \n 2. Display OS info \n 
         });
     }
 
-
-
 //Read package.json
+    function readPackageJson(){
+        fs.readFile(__dirname +'/package.json', 'utf-8', (err,content) => {
+            console.log(content)
+        });
+    }
 
 // Display OS info
+    function readOSInfo(){
 
-
-
-//if choosing an invalid number
+    }
